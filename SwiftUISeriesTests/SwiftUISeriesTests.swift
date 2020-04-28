3ce6eae025f37b562f8a0df579e9ff239508f14d
Ibnu Muzzakkir <ibnu.muzzakkir@amarbank.co.id>
2020-04-28 11:41:16 +0700

    Initialize Project

---
+++ SwiftUISeriesTests/SwiftUISeriesTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftUISeriesTests.swift
+//  SwiftUISeriesTests
+//
+//  Created by Mobile Dev on 28/04/20.
+//  Copyright © 2020 SCO Project. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftUISeries
+
+class SwiftUISeriesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
